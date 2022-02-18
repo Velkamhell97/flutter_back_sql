39783e2e8f0725a07af7ec9275e5d804d9253448
Luis Daniel Valencia <danielvalencia97@gmail.com>
2022-02-18 11:54:11 -0500

    back_sql

--- database/connection.ts
+++ database/connection.ts
@@ -1,5 +1,7 @@
 import { Sequelize } from 'sequelize';
 
+//-estos archivos se ejecutan antes que las variables de entorno, por lo que se tendria que inicializar en otro lugar
+//-o despues de la declaracion del prrocess o por medio de una funcion
 const db = new Sequelize('flutter_chat','root','', {
   host:'localhost',
   dialect: 'mysql',
